558e786b2ef47fc381c5a66bb8af9add19dc09fe
Tiago Aguiar <tyago_tka48@hotmail.com>
2021-01-21 21:40:36 -0300

    refatoração no HomePageTest

--- src/test/java/com/qa/hubspot/tests/HomePageTest.java
+++ src/test/java/com/qa/hubspot/tests/HomePageTest.java
@@ -9,9 +9,12 @@ import com.qa.hubspot.pages.LoginPage;
 
 public class HomePageTest extends BaseTest {
 
+	private static final String SENHA = "Test@1234";
+	private static final String EMAIL = "naveenanimation20@gmail.com";
+
 	@Test(priority = 1)
 	public void homePageTitleTest() {
-		page.getInstance(LoginPage.class).logar("naveenanimation20@gmail.com", "Test@1234");
+		page.getInstance(LoginPage.class).logar(EMAIL, SENHA);
 		String title = page.getInstance(HomePage.class).getHomePagetitle();
 		System.out.println("O título da Homepage é: " + title);
 		assertEquals(title, "Reports dashboard");
@@ -19,7 +22,7 @@ public class HomePageTest extends BaseTest {
 	
 	@Test
 	public void verificarHomePageHeaderTest() {
-		page.getInstance(LoginPage.class).logar("naveenanimation20@gmail.com", "Test@1234");
+		page.getInstance(LoginPage.class).logar(EMAIL, SENHA);
 		String header = page.getInstance(HomePage.class).getHomePageHeader();
 		System.out.println(header);
 		assertEquals(header, "Sales Dashboard");	
